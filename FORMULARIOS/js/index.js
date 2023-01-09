51996d950d8b7178f148ce6103bf0ba10e18249b
Rafael Laube <laube.developer@gmail.com>
2023-01-09 17:16:08 -0300

    Alterações Finais

--- FORMULARIOS/js/index.js
+++ FORMULARIOS/js/index.js
@@ -1,21 +1,9 @@
-//dev options
-const devmode = false
-
-if(devmode){
-    const inputsList = document.getElementsByTagName("input")
-
-    for(let index = 0; index < inputsList.length; index ++ ){
-        inputsList[index].removeAttribute("required")
-    }
-}
-
-// end dev options
 const form = document.querySelector("form")
 const selectBairro = document.querySelector("input[type=list]")
 const datalist = document.querySelector("#bairro")
 
-//lista de bairros
-//pode ser buscada por uma api que retorna a
+//LISTA DE BAIRROS
+//Ela pode ser buscada por uma api que retorna a
 //lista atualizada dos bairros disponíveis
 const bairros = [
     "Taguatinga",
@@ -37,13 +25,13 @@ function mostrarBairros(){
 form.onsubmit = (e)=>{
     e.preventDefault()
 
+    //Verificar bairro
     if(!bairros.includes(selectBairro.value)){
         alert("Selecione um bairro dentre os listados!")
         return
     }
 
     form.submit()
-
 }
 
 mostrarBairros()
